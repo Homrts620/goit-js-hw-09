b4c1e3c3ca0e3baffc2dfc1a6c23429c6ebded2c
Homrts620 <hom620@gmail.com>
2024-02-24 17:34:02 +0100

    hw

---
+++ src/js/03-promises.js
@@ -0,0 +1,34 @@
+const myForm = document.querySelector('.form');
+
+myForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const delay = Number(e.target.elements[0].value);
+    const step = Number(e.target.elements[1].value);
+    const amount = Number(e.target.elements[2].value);
+
+    for (let i = 0; i < amount; i++) {
+        createPromise(i, delay + i * step)
+        .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+})
+        .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+});
+    }
+})
+
+
+
+function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const shouldResolve = Math.random() > 0.3;
+
+            if (shouldResolve) {
+            resolve({ position, delay });
+            } else {
+            reject({ position, delay });
+        }
+    }, delay);
+    });
+}
